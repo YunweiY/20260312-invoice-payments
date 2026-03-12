5fefed281f5b0412315ea5076131fcce2e198b5d
Yunwei <yunweiy0211@gmail.com>
2026-03-12 17:53:11 -0400

    chore: format frontend codebase with prettier

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -2,10 +2,10 @@ import { createBrowserRouter } from 'react-router-dom';
 import Index from '@/pages/index';
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Index />,
-    },
+  {
+    path: '/',
+    element: <Index />,
+  },
 ]);
 
 export default router;
